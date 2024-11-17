a8ad1646c88e86bf4becf6516637b29c758a7c37
balendu9 <balendurawat9@gmail.com>
2024-11-17 20:03:37 +0530

    updates

--- app/components/Hero-section/Hero.tsx
+++ app/components/Hero-section/Hero.tsx
@@ -28,8 +28,8 @@ const Hero = (props: Props) => {
                         <div className='flex items-center p-3 px-5'>
                             <img className=" d-block w-auto flex-grow-0 flex-shrink-0 mr-6 max-md:mr-3 scale-110" width="44" height="44" loading="lazy" decoding="async" alt="" aria-hidden="true" src="https://github.githubassets.com/images/modules/site/eyebrow-banner-icon-copilot-x.svg" />
                             <div className='md:pr-5 pr-3 md:mr-2 md:pl-2'>
-                                <div className='font-medium text-white text-[16px] max-md:text-[14px] leading-5'>Introducing GitHub Copilot X</div>
-                                <div className='text-neutral-500 max-md:text-[14px] text-[16px]'>Your Ai pair programmer is leveling Up</div>
+                                <div className='font-medium text-white text-[16px] max-md:text-[14px] leading-5'>Introducing the smoothest gameplay, ever seen in web3</div>
+                                <div className='text-neutral-500 max-md:text-[14px] text-[16px]'>Its time to level up the web3 gaming community</div>
                             </div>
                             <div className='ml-auto'>
                                 <svg xmlns="http://www.w3.org/2000/svg" className={`  text-white transition ease-in duration-150 ${hovered ? "translate-x-0 ": "-translate-x-1"}`} width="16" height="16" viewBox="0 0 16 16" fill="none"><path fill="currentColor" d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path><path className={` text-white transition ease-in duration-150 ${hovered ? " opacity-100" : "opacity-0 "}`} stroke="currentColor" d="M1.75 8H11" strokeWidth="1.5" strokeLinecap="round"></path></svg>
